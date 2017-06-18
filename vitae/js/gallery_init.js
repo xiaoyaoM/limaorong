e548fe7794a7f27c80462666a63a35b73d3c4455
xiaoyaoM <1292666914@qq.com>
2017-06-19 02:37:45 +0800

    Curriculum vitae

---
+++ vitae/js/gallery_init.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$('#ARoundabout').roundabout({
+		 shape: 'figure8',
+		 minOpacity: 1
+	});
+});
+
+$(document).ready(function() {
+	$('#BRoundabout').roundabout({
+		 shape: 'figure8',
+		 minOpacity: 1
+	});
+});
